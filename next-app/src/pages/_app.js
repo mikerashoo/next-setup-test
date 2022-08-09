cfb07d6cd6aab610180f5807bfe764330da8ec7d
Mikias Birhanu <37136032+mikerashoo@users.noreply.github.com>
2022-08-09 19:59:10 +0300

    folder deletion

--- next-app/src/pages/_app.js
+++
@@ -1,9 +0,0 @@
-
-import 'tailwindcss/tailwind.css'
-import { wrapper } from '../lib/store.js';
-
-const App = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />;
-};
-
-export default wrapper.withRedux(App);
